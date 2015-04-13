fc99024431ae60913d4e3e25909049f76f436596
David Feng <jiguanglizipao@163.com>
2015-04-14 00:23:37 +0800

    Include ZodiacGraph https://github.com/clemenssielaff/ZodiacGraph

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
+
 #include <QApplication>
 
+///
+/// \brief Main function of this application.
+///
+/// \param [in] argc    The number of strings pointed to by argv.
+/// \param [in] argv    Name of the programm + misc.
+///
+/// \return             0 if the application ran successfully, otherwise an error code.
+///
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Mainwindow w;
-    w.show();
+    // create application
+    QApplication app(argc, argv);
+    app.setOrganizationName("clemens-sielaff");
+    app.setOrganizationDomain("www.clemens-sielaff.com");
+    app.setApplicationName("ZodiacGraph_ExampleApp");
 
-    return a.exec();
+    // create the main window and enter the main execution loop
+    MainWindow window;
+    window.show();
+    int result = app.exec();
+
+    return result;
 }
